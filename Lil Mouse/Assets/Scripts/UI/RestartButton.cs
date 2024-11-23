bb2cabd6296017935c44d08f4331af8092d43d50
Leonie <leonie@wargitsch.de>
2024-11-23 09:33:26 +0100

    Main Menu and Networking Scene

--- Lil Mouse/Assets/Scripts/UI/RestartButton.cs
+++ Lil Mouse/Assets/Scripts/UI/RestartButton.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour
 {
+    [SerializeField] private bool m_loadActiveScene = true;
+    [SerializeField] private string m_nextSceneName;
     public void RestartGame() 
     {
-    	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (m_loadActiveScene)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if(SceneManager.GetSceneByName(m_nextSceneName) != null)
+        {
+            SceneManager.LoadScene(m_nextSceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Failed to load Scene with Name " + m_nextSceneName);
+        }
     }
 }
